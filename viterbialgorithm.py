fdf5d4e0afc22ea94d5ddfee334ba4d6f1886647
BeaLove <67667545+BeaLove@users.noreply.github.com>
2020-09-13 19:00:37 +0200

    Add files via upload

--- viterbialgorithm.py
+++ viterbialgorithm.py
@@ -22,33 +22,30 @@ def viterbi(pi, a, b, obsv):
     firstobsv = obsv[0] #the first observation
     delta0 = []
     for i in range(len(b)):
-        delta0.append(pi[i]*b[i][firstobsv]) ##initialize probability of first observation given initial probabilities
+        delta0.append(pi[i]*b[i][firstobsv])##initialize probability of first observation given initial probabilities
+    #print('delta1', delta0)
     viterbi = [[0 for i in range(len(a))] for j in range(len(obsv))] #empty viterbi matrix
     viterbi[0] = delta0 #add delta0 to viterbi matrix
-    backpointers = [[0 for i in range(len(a))] for j in range(len(obsv))] #initialize empty backpointers
+    backpointers = [[9 for i in range(len(a))] for j in range(len(obsv))] #initialize empty backpointers
     for t in range(1, len(obsv)): ##recursive step
         currentObsv = obsv[t]
         for i in range(len(delta0)):
             #here, instead of summing all probabilities, we create a list for every
-            #column of the transition matrix a, and fetch the maximum probability (most likely current state)
+            #column of the transition matrix a, and fetch the maximum probability (most likely previous state)
             probabilities = [] #empty probabilities list
             for j in range(len(a[0])):
                 probabilities.append(viterbi[t-1][j]*a[j][i]*b[i][currentObsv]) #for every column in a
-            viterbi[t][i] = max(probabilities) #keep only the max from the list created, instead of the sum
-            backpointers[t][i] = probabilities.index(max(probabilities)) #save argmax to backpointers
+            viterbi[t][i] = max(probabilities) #most likely transition #keep only the max from the list created, instead of the sum
+            backpointers[t][i] = probabilities.index(max(probabilities)) #most likely previous state
 
-    #print("viterbi:", viterbi)
-    #print("backpointers", backpointers)
-    bestpathprob = max(viterbi[-1]) #max probability from the last iteration
-    bestpath = []
-    #### NOTE: below code doesnt work (entire state sequence gets returned but one step behind
-    #### eg 0121 becomes 0012
-    #for i in range(len(backpointers)): #get the most likely state sequence from saved backpointers
-        #bestpath.append(backpointers[i].index(max(backpointers[i])))
-    ### THIS WORKS but doesnt make use of "backpointers" at all so clearly incorrect in that it doesn't follow
-    ### the correct viterbi algorithm pseudocode
-    for i in range(len(viterbi)):
-        bestpath.append(viterbi[i].index(max(viterbi[i])))
+    bestpathprob = max(viterbi[-1]) #max probability from the last iteration = total probability of this path
+    laststate = viterbi[-1].index(max(viterbi[-1]))
+    bestpath = [0 for i in range(len(obsv))]
+    bestpath[0] = laststate
+    for i in range(1, len(obsv)):
+        bestpath[i] = backpointers[-i][laststate]
+        laststate = bestpath[i]
+    bestpath.reverse()
     return bestpathprob, bestpath
 
 def output(list):
@@ -58,26 +55,22 @@ def output(list):
     string = string[:-1]
     return string
 
-input = open("hmm3_01.in", "r")
-avals = input.readline().split()
-#avals = input().split()
+#input = open("sample1_RK.in", "r")
+#avals = input.readline().split()
+avals = input().split()
 a, arows, acols = matrices(avals)
-bvals = input.readline().split()
-#bvals = input().split()
+#bvals = input.readline().split()
+bvals = input().split()
 b, brows, bcols = matrices(bvals)
-pivals = input.readline().split()
-#pivals = input().split()
+#pivals = input.readline().split()
+pivals = input().split()
 pi, pirows, picols = matrices(pivals)
 pi = pi[0]
-obsv = input.readline().split()
-#obsv = input().split()
+#obsv = input.readline().split()
+obsv = input().split()
 noObsv = obsv.pop(0)
 obsv = [int(i) for i in obsv]
 #input.close()
-
 bestpathprob, bestpath = viterbi(pi, a, b, obsv)
 bestpathformat = output(bestpath)
-
-#answer = str(forward).strip(" ")
-#print(bestpathprob)
 print(bestpathformat)
